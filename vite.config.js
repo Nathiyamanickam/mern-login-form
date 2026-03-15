e4bbf138ec7432f142cda7d25c60b95adaacc490
Nathiya <nathiyamanickam6181@gmail.com>
2026-03-15 15:37:37 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/mern-login-form/"
+  plugins: [react()],
+  base: '/mern-login-form/',
 })

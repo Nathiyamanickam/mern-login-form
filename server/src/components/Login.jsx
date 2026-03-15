aea826d4f85ab6e05fd66b707255bef315029bc9
Nathiya <nathiyamanickam6181@gmail.com>
2026-03-15 10:11:18 +0530

    final live

--- server/src/components/Login.jsx
+++ server/src/components/Login.jsx
@@ -1,40 +1,32 @@
 import { useState } from "react";
 
-export default function Signup({ onSignup }) {
-  const [name, setName] = useState("");
+export default function Login({ onLogin }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSignup = async (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("https://mern-login-form-2.onrender.com/api/users/signup", {
+    const res = await fetch("http://localhost:5000/api/users/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ email, password }),
     });
 
     const data = await res.json();
     alert(data.message);
 
     if (res.ok) {
-      onSignup(); 
+      onLogin(); 
     }
   };
 
   return (
-    <form onSubmit={handleSignup}>
-
-
-      <h2>Signup</h2>
-
-
-      <input
-        placeholder="Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-
+   
+    <form onSubmit={handleLogin}>
+   
+     
+<h2>Login</h2>
       <input
         placeholder="Email"
         value={email}
@@ -48,7 +40,7 @@ export default function Signup({ onSignup }) {
         onChange={(e) => setPassword(e.target.value)}
       />
 
-      <button type="submit">Signup</button>
+      <button type="submit">Login</button>
     </form>
   );
 }
